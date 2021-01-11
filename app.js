99ae2bd2aca20e9a282af98256d57bd66799abb2
DillanR1 <dillanroby@yahoo.com>
2021-01-10 22:44:18 -0800

    api is working, returns valid response object

--- app.js
+++ app.js
@@ -1,19 +1,5 @@
-class Weather {
-    constructor(city, state) {
-        this.apiKey = '9f046df65f8f779272f884474d8c48a0';
-        this.city = city;
-        this.state = state;
-    }
+const weather = new Weather('Oakley, CA');
 
-    // Fetch weather from API
-    async getWeather() {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.state}&units=imperial&appid=${this.apiKey}&`);
-    
-        const responseData = await response.json();
-
-        console.log(responseData);
-
-        return responseData;
-    
-    }
-}
+weather.getWeather()
+    .then(results => {})
+    .catch(err => console.log(err));
