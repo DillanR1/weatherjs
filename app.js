7f0d74a0f1b5b086100dc49da7d69573b371dc97
DillanR1 <dillanroby@yahoo.com>
2021-01-11 01:47:36 -0800

    safety commit

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const weather = new Weather('Oakley, CA');
 
-weather.getWeather()
-    .then(results => {})
+// Get weather on Dom load
+document.addEventListener('DomContentLoaded', getWeather);
+
+//NOTE weather.changeLocation('London'); currently working, refactor to change dynamically 
+
+function getWeather(){
+  weather.getWeather()
+    .then(results => {
+       ui.paint(results)
+    })
     .catch(err => console.log(err));
+}
