c84421bd2b3b53ec08df7b2be571cc289965a993
DillanR1 <dillanroby@yahoo.com>
2021-01-12 03:05:51 -0800

    API call may be incorrect, app displaying no data

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
-const weather = new Weather('Oakley, CA');
+const storage = new Storage();
+
+const weatherLocation = storage.getLocationData();
+
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
+
+const ui = new UI();
 
 // Get weather on Dom load
 document.addEventListener('DomContentLoaded', getWeather);
 
-//NOTE weather.changeLocation('London'); currently working, refactor to change dynamically 
+
+
+// Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value;
+  const state = document.getElementById('state').value;
+
+  weather.changeLocation(city);
+
+  storage.setLocationData(city);
+
+    // Get and display weather
+    getWeather();
+
+    //Close modal
+    $('#locModal').modal('hide');
+ });
 
 function getWeather(){
   weather.getWeather()
     .then(results => {
-       ui.paint(results)
+       ui.paint(results);
     })
     .catch(err => console.log(err));
 }
